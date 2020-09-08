20980569bb29527d4f36b22c46bb1728a84e363b
gunjan surana <g@gmail.com>
2020-09-08 09:05:25 +0000

    new feature release

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! from the feature branch"); 
+        System.out.println("Hello, World! from the newfeature branch"); 
     }
 }

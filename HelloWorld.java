c169df591abe5e17d638a61d1555401f15a9be4d
gunjan surana <g@gmail.com>
2020-09-08 11:32:36 +0000

    Jenkins 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! from the newfeature branch with Jenkins"); 
+        System.out.println("Hello, World! from the newfeature branch with Jenkins 2"); 
     }
 }
